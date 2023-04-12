bb74e0b4e007c479786fa160cb8e9498d56b9fbe
Joyining <yining1204@gmail.com>
2023-04-12 17:21:11 +0800

    Set authentication

--- rest_framework_project/views.py
+++ rest_framework_project/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from my_app.serializers import ProductSerializer
 from my_app.models import Product
 
 
 class ProductView(APIView):
+
+    # allow anybody
+    # permission_classes = ()
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         query_set = Product.objects.all()
-        serializer = ProductSerializer(query_set, many=True)
+        # get all products
+        # serializer = ProductSerializer(query_set, many=True)
+
+        # get the first product
+        first_product = query_set.first()
+        serializer = ProductSerializer(first_product)
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
