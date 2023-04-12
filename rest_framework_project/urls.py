5e97fa356b40e8fda43db85465aa84f15fbf6e38
Joyining <yining1204@gmail.com>
2023-04-12 17:25:00 +0800

    Create token when login

--- rest_framework_project/urls.py
+++ rest_framework_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import ProductView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', ProductView.as_view(), name='product'),
+    path('api/token/', obtain_auth_token, name='token')
 ]
